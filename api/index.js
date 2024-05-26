eee7865f06378e67035150a264458256977d1ac5
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2024-05-26 02:49:54 -0600

    post endpoint

---
+++ api/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = 7000;
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/csf_skillassessment')
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB!:', err));
+
+// Middleware
+app.use(express.json()); // Parse JSON request bodies
+
+// Test route
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
+
+// Post Endpoint
+app.post('/starwars_form', async (req, res) => {
+    try {
+        // Takes in and stores form (should return ID)
+        res.status(201).send();
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
+
+
+
+
